a30e98245a780f6709f44d4452fa8a9311c05957
Andrea Mazzini <andrea.mazzini@gmail.com>
2015-03-18 15:48:33 +0100

    Initial commit

---
+++ Demo/GearAnimation/ViewController.swift
@@ -0,0 +1,141 @@
+//
+//  ViewController.swift
+//  GearAnimation
+//
+//  Created by Andrea Mazzini on 18/03/15.
+//  Copyright (c) 2015 Fancy Pixel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var refreshContainerView: UIView!
+    var isRefreshControlAnimating = false
+    var centerGear: MainGear!
+    var topGear: BigGear! // cue Jessica from the Allman brothers
+    var rightGear: BigGear!
+    var bottomGear: BigGear!
+    var leftGear: BigGear!
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("Cell")! as UITableViewCell
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.setupRefreshControl()
+    }
+
+    func setupRefreshControl() {
+
+        self.refreshControl = UIRefreshControl()
+
+        self.refreshContainerView = UIView(frame: self.refreshControl!.bounds)
+        self.refreshContainerView.backgroundColor = UIColor(red:0.13, green:0.29, blue:0.55, alpha:1)
+
+        self.centerGear = MainGear(frame: CGRectMake(0, 0, 48, 48))
+        self.centerGear.backgroundColor = UIColor.clearColor()
+        self.centerGear.tintColor = UIColor(red:0.36, green:0.52, blue:0.92, alpha:1)
+        self.centerGear.center = CGPoint(x: CGRectGetMidX(self.refreshContainerView.frame), y: CGRectGetMidY(self.refreshContainerView.frame))
+
+        self.topGear = BigGear(frame: CGRectMake(0, 0, 92, 92))
+        self.topGear.backgroundColor = UIColor.clearColor()
+        self.topGear.tintColor = UIColor(red:0.31, green:0.47, blue:0.84, alpha:1)
+        self.topGear.center = CGPoint(x: CGRectGetMidX(self.refreshContainerView.frame) + 48, y: CGRectGetMidY(self.refreshContainerView.frame) - 49)
+
+        self.rightGear = BigGear(frame: CGRectMake(0, 0, 92, 92))
+        self.rightGear.backgroundColor = UIColor.clearColor()
+        self.rightGear.tintColor = UIColor(red:0.23, green:0.41, blue:0.76, alpha:1)
+        self.rightGear.center = CGPoint(x: CGRectGetMidX(self.refreshContainerView.frame) + 120, y: CGRectGetMidY(self.refreshContainerView.frame))
+
+        self.bottomGear = BigGear(frame: CGRectMake(0, 0, 88, 88))
+        self.bottomGear.backgroundColor = UIColor.clearColor()
+        self.bottomGear.tintColor = UIColor(red:0.31, green:0.47, blue:0.84, alpha:1)
+        self.bottomGear.center = CGPoint(x: CGRectGetMidX(self.refreshContainerView.frame) - 48, y: CGRectGetMidY(self.refreshContainerView.frame) + 42)
+
+        self.leftGear = BigGear(frame: CGRectMake(0, 0, 92, 92))
+        self.leftGear.backgroundColor = UIColor.clearColor()
+        self.leftGear.tintColor = UIColor(red:0.23, green:0.41, blue:0.76, alpha:1)
+        self.leftGear.center = CGPoint(x: CGRectGetMidX(self.refreshContainerView.frame) - 110, y: CGRectGetMidY(self.refreshContainerView.frame) - 18)
+
+        self.refreshContainerView.addSubview(self.topGear)
+        self.refreshContainerView.addSubview(self.rightGear)
+        self.refreshContainerView.addSubview(self.bottomGear)
+        self.refreshContainerView.addSubview(self.leftGear)
+        self.refreshContainerView.addSubview(self.centerGear)
+        
+        self.refreshContainerView.clipsToBounds = true
+        self.refreshControl!.tintColor = UIColor.clearColor()
+
+        self.refreshControl!.addSubview(self.refreshContainerView)
+        self.refreshControl?.addTarget(self, action: "refresh", forControlEvents: UIControlEvents.ValueChanged)
+    }
+
+    func refresh(){
+        var popTime = dispatch_time(DISPATCH_TIME_NOW, Int64(3.0 * Double(NSEC_PER_SEC)));
+        dispatch_after(popTime, dispatch_get_main_queue()) { () -> Void in
+            self.refreshControl!.endRefreshing()
+        }
+    }
+
+    override func scrollViewDidScroll(scrollView: UIScrollView) {
+
+        var refreshBounds = self.refreshControl!.bounds;
+
+        // Distance the table has been pulled
+        var pullDistance = max(0.0, -self.refreshControl!.frame.origin.y);
+        var pullRatio = min(max(pullDistance, 0.0), 140.0) / 140.0;
+
+        self.centerGear.center = CGPoint(x: CGRectGetMidX(self.refreshContainerView.frame), y: pullDistance / 2)
+        self.topGear.center = CGPoint(x: CGRectGetMidX(self.refreshContainerView.frame) + 48, y: pullDistance / 2 - 49)
+        self.rightGear.center = CGPoint(x: CGRectGetMidX(self.refreshContainerView.frame) + 120, y: pullDistance / 2)
+        self.bottomGear.center = CGPoint(x: CGRectGetMidX(self.refreshContainerView.frame) - 47, y: pullDistance / 2 + 46)
+        self.leftGear.center = CGPoint(x: CGRectGetMidX(self.refreshContainerView.frame) - 105, y: pullDistance / 2 - 17)
+
+        refreshBounds.size.height = pullDistance;
+
+        self.refreshContainerView.frame = refreshBounds;
+
+        // Don't rotate the gears if the refresh animation is playing
+        if (!self.refreshControl!.refreshing && !self.isRefreshControlAnimating) {
+            self.centerGear.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_2) * pullRatio)
+            self.topGear.transform = CGAffineTransformMakeRotation(0.025 - 0.9 * pullRatio)
+            self.rightGear.transform = CGAffineTransformMakeRotation(-0.03 + 0.9 * pullRatio)
+            self.bottomGear.transform = CGAffineTransformMakeRotation(0.025 - 0.9 * pullRatio)
+            self.leftGear.transform = CGAffineTransformMakeRotation(0.025 + 0.9 * pullRatio)
+        }
+
+        // If we're refreshing and the animation is not playing, then play the animation
+        if (self.refreshControl!.refreshing && !self.isRefreshControlAnimating) {
+            self.animateRefreshView()
+        }
+    }
+
+    func animateRefreshView() {
+        self.isRefreshControlAnimating = true
+
+        UIView.animateWithDuration(0.3, delay: 0, options: .CurveLinear, animations: {
+            self.centerGear.transform = CGAffineTransformRotate(self.centerGear.transform, CGFloat(M_PI_2))
+            self.topGear.transform = CGAffineTransformRotate(self.topGear.transform, -CGFloat(M_PI_2))
+            self.rightGear.transform = CGAffineTransformRotate(self.rightGear.transform, CGFloat(M_PI_2))
+            self.bottomGear.transform = CGAffineTransformRotate(self.bottomGear.transform, -CGFloat(M_PI_2))
+            self.leftGear.transform = CGAffineTransformRotate(self.leftGear.transform, CGFloat(M_PI_2))
+            }, completion: { finished in
+                // If still refreshing, keep spinning
+                if (self.refreshControl!.refreshing) {
+                    self.animateRefreshView()
+                } else {
+                    self.isRefreshControlAnimating = false
+                }
+            }
+        )
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,32 @@
+# GearRefreshControl
+A custom animation for the UIRefreshControl. Inspired by this [Dribble shot](https://dribbble.com/shots/1974767-gear-powered-pull-to-refresh-animation?list=animated&sort=popular&timeframe=now&offset=19) and [this tutorial](http://www.jackrabbitmobile.com/design/ios-custom-pull-to-refresh-control/). 
+
+# Usage
+Right now it's just a demo, I might make a library out of it, but I don't see the point right now, let me know in the Issue section if you are interested. 
+
+# Screenshot
+![GearRefreshControl](https://raw.githubusercontent.com/andreamazz/GearRefreshControl/master/assets/screenshot.gif)
+
+#MIT License
+
+	Copyright (c) 2015 Andrea Mazzini. All rights reserved.
+
+	Permission is hereby granted, free of charge, to any person obtaining a
+	copy of this software and associated documentation files (the "Software"),
+	to deal in the Software without restriction, including
+	without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to
+	permit persons to whom the Software is furnished to do so, subject to
+	the following conditions:
+
+	The above copyright notice and this permission notice shall be included
+	in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+	CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	
